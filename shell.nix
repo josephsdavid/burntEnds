c08351f99f05eaa7bf4cad0add7a12b3f7b43734
josephsdavid <josephsd@smu.edu>
2019-09-05 15:37:02 -0500

    playing with symlinks

--- shell.nix
+++ shell.nix
@@ -1,1 +1,17 @@
-../shell.nix
+
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    name = "ML";
+    buildInputs = with pkgs; [
+      python37
+      python37Packages.numpy
+      python37Packages.scikitlearn
+      python37Packages.pip
+      python37Packages.virtualenv
+    ];
+   shellHook = ''
+      '';
+
+  }
